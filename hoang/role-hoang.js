0707cdf9568e817fe98e6a6306035fa876cb6d55
huihoang <hoang.nguyen2211093@hcmut.edu.vn>
2024-04-05 00:43:18 +0700

    add achive service form-contact footer map

--- hoang/role-hoang.js
+++ hoang/role-hoang.js
@@ -3,7 +3,9 @@ const clickSearch = document.querySelector('.ti-search')
 const jsSearch = document.querySelector('.js-show-search')
 const jsHides = document.querySelectorAll('.js-hide')
 const jsRight = document.querySelector('.js-right')
-const closeSearch = document.querySelector('#slider, #container, #logo')
+const closeSomething = document.querySelector('#main')
+const propagationSearch_1 = document.querySelector('.ti-search')
+const propagationSearch_2 = document.querySelector('.js-show-search')
 
 const clickInfo = document.querySelector('.js-info')
 const jsInfo = document.querySelector('#info-personal')
@@ -12,9 +14,30 @@ const clickSave = document.querySelector('.js-saveInfo')
 const clickHome = document.querySelector('#back-home') //chinh lai ve home dung nghia
 
 
-const clickLogin = document.querySelector('.js-login, .js-logout') // thoat all
+const clickLogin = document.querySelector('.js-login')
 const jsLogin = document.querySelector('#modal-login')
-const closeLogin = document.querySelector('#navigation')
+const propagationLogin_1 = document.querySelector('.background-login')
+const propagationLogin_2 = document.querySelector('.js-login')
+const propagationLogin_3 = document.querySelector('#info-summary')
+const close = document.querySelector('.ti-close')
+
+
+const clickLogout = document.querySelector('.js-logout') // thoat all chưa xong
+
+const clickContact = document.querySelector('.js-contact')
+const jsContact = document.querySelector('#sent-info')
+const clickSent = document.querySelector('.js-sent')
+
+const clickForgetMK = document.querySelector('.js-forgetmk')
+
+
+const clickService = document.querySelector('.js-service')
+const jsService = document.querySelector('.Services')
+const jsAchievement = document.querySelector('.Achievements')
+const clickAchievement = document.querySelector('.js-achievement')
+
+
+
 
 
 
@@ -40,21 +63,33 @@ function showInfo() {
     jsInfo.classList.add('show')
 }
 
-function backHome(){
+function backHome() {
     jsInfo.classList.remove('show')
 }
 
-function showLogin(){
+function showLogin() {
     jsLogin.classList.add('show')
 }
-function hideLogin(){
+function hideLogin() {
     jsLogin.classList.remove('show')
 }
 
+function showContact(){
+    jsContact.classList.add('show')
+}
+function hideContact(){
+    jsContact.classList.remove('show')
+}
 
 
 clickSearch.addEventListener('click', showSearch)
-closeSearch.addEventListener('click', hideSearch)
+propagationSearch_1.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+propagationSearch_2.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+closeSomething.addEventListener('click', hideSearch)
 
 clickSave.addEventListener('click', saveInfo)
 clickInfo.addEventListener('click', showInfo)
@@ -62,4 +97,47 @@ clickInfo.addEventListener('click', showInfo)
 clickHome.addEventListener('click', backHome)
 
 clickLogin.addEventListener('click', showLogin)
-closeLogin.addEventListener('click', hideLogin)
+close.addEventListener('click', hideLogin)
+closeSomething.addEventListener('click', hideLogin)
+propagationLogin_1.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+propagationLogin_2.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+propagationLogin_3.addEventListener('click', function (event) {
+    event.stopPropagation()
+})
+clickForgetMK.addEventListener('click', function(){
+    alert("Gọi vào số quản trị viên: 0819.170.504")
+})
+
+
+clickContact.addEventListener('click', showContact)
+closeSomething.addEventListener('click', hideContact)
+clickContact.addEventListener('click', function(event){
+    event.stopPropagation()
+})
+jsContact.addEventListener('click', function(event){
+    event.stopPropagation()
+})
+clickSent.addEventListener('click', function(){
+    alert("Đã gửi thành công")
+})
+
+
+clickService.addEventListener('click', function(){
+    jsService.classList.add('show')
+    jsAchievement.classList.add('hide')
+})
+clickAchievement.addEventListener('click', function(){
+    jsService.classList.remove('show')
+    jsAchievement.classList.remove('hide')
+})
+
+
+
+
+
+
+
