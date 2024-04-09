661cc032fe66c2ad8fe1b1a258cacb400fa63e2d
huihoang <hoang.nguyen2211093@hcmut.edu.vn>
2024-04-10 00:04:37 +0700

    api info-bacsi and media responsive

--- hoang/list-doctor.js
+++ hoang/list-doctor.js
@@ -8,7 +8,7 @@ async function getDataFromPublicAPI() {
     for(let i=3; i < results.length; i+=3){
         const ulItem = document.createElement('ul')
         ulItem.innerHTML = `
-            <li class="avatar" style=" background-image: url(${results[i-3].picture.thumbnail});">
+            <li class="avatar" style=" background-image: url(${results[i-3].picture.large});">
             <p>
                 <span>${results[i-3].location.city}</span>
                 ${results[i-3].name.title}.${results[i-3].name.first} ${results[i-3].name.last}
@@ -16,7 +16,7 @@ async function getDataFromPublicAPI() {
                 <span>${results[i-3].location.city}</span>
             </p>
             </li>
-            <li class="avatar" style=" background-image: url(${results[i-2].picture.thumbnail});">
+            <li class="avatar" style=" background-image: url(${results[i-2].picture.large});">
             <p>
                 <span>${results[i-2].location.city}</span>
                 ${results[i-2].name.title}.${results[i-2].name.first} ${results[i-2].name.last}
@@ -24,7 +24,7 @@ async function getDataFromPublicAPI() {
                 <span>${results[i-2].location.city}</span>
             </p>
             </li>
-            <li class="avatar" style=" background-image: url(${results[i-1].picture.thumbnail});">
+            <li class="avatar" style=" background-image: url(${results[i-1].picture.large});">
             <p>
                 <span>${results[i-1].location.city}</span>
                 ${results[i-1].name.title}.${results[i-1].name.first} ${results[i-1].name.last}
--- hoang/role-hoang.js
+++ hoang/role-hoang.js
@@ -153,3 +153,6 @@ document.getElementById('prevSv').addEventListener('click', function() {
 });
 
   
+
+
+
