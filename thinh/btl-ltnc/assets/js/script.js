ee98433b7d5cdd51c1fbd6066a8f7b581eb67722
thinh-huynh2004 <thinh.huynhhctbk22@hcmut.edu.vn>
2024-04-16 11:20:06 +0700

    change

---
+++ thinh/btl-ltnc/assets/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var myButton = document.getElementById("mybutton");
+    var myLink = document.getElementById("info");
+
+    myButton.addEventListener("click", function(event) {
+        event.stopPropagation(); // Ngăn chặn sự kiện click trên nút button lan truyền đến body hoặc document
+        if (myLink.style.display === "none") {
+            myLink.style.display = "block"; // Hiển thị link nếu đang bị ẩn
+        } else {
+            myLink.style.display = "none"; // Ẩn link nếu đang hiển thị
+        }
+    });
+
+    document.addEventListener("click", function(event) {
+        if (event.target !== myButton && myLink.style.display !== "none") {
+            myLink.style.display = "none"; // Ẩn link khi click vào bất kỳ nơi nào ngoài nút button
+        }
+    });
+});
