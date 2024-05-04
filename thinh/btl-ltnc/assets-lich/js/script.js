afd5078deb7a3e64d692988f0fc0d271558b27dd
thinh-huynh2004 <thinh.huynhhctbk22@hcmut.edu.vn>
2024-05-04 16:37:19 +0700

    js

--- thinh/btl-ltnc/assets-lich/js/script.js
+++ thinh/btl-ltnc/assets-lich/js/script.js
@@ -38,22 +38,22 @@ function addDataToScheduleTable(lichLamViec) {
 // Gọi hàm và truyền biến lichLamViec vào
 addDataToScheduleTable(lichLamViec);
 
-document.addEventListener("DOMContentLoaded", function() {
-    var myButton = document.getElementById("mybutton");
-    var myLink = document.getElementById("info");
+// document.addEventListener("DOMContentLoaded", function() {
+//     var myButton = document.getElementById("mybutton");
+//     var myLink = document.getElementById("info");
 
-    myButton.addEventListener("click", function(event) {
-        event.stopPropagation(); // Ngăn chặn sự kiện click trên nút button lan truyền đến body hoặc document
-        if (myLink.style.display === "none") {
-            myLink.style.display = "block"; // Hiển thị link nếu đang bị ẩn
-        } else {
-            myLink.style.display = "none"; // Ẩn link nếu đang hiển thị
-        }
-    });
+//     myButton.addEventListener("click", function(event) {
+//         event.stopPropagation(); // Ngăn chặn sự kiện click trên nút button lan truyền đến body hoặc document
+//         if (myLink.style.display === "none") {
+//             myLink.style.display = "block"; // Hiển thị link nếu đang bị ẩn
+//         } else {
+//             myLink.style.display = "none"; // Ẩn link nếu đang hiển thị
+//         }
+//     });
 
-    document.addEventListener("click", function(event) {
-        if (event.target !== myButton && myLink.style.display !== "none") {
-            myLink.style.display = "none"; // Ẩn link khi click vào bất kỳ nơi nào ngoài nút button
-        }
-    });
-});
+//     document.addEventListener("click", function(event) {
+//         if (event.target !== myButton && myLink.style.display !== "none") {
+//             myLink.style.display = "none"; // Ẩn link khi click vào bất kỳ nơi nào ngoài nút button
+//         }
+//     });
+// });
