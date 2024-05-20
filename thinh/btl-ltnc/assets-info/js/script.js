9e2bd765e3e9f7fb22ce3483b2d46fbdb1f9bee3
thinh-huynh2004 <thinh.huynhhctbk22@hcmut.edu.vn>
2024-05-20 16:15:14 +0700

    addinfo

---
+++ thinh/btl-ltnc/assets-info/js/script.js
@@ -0,0 +1,27 @@
+var info = {
+    fullName: "Nguyễn Văn A",
+    identityNumber: "123456789",
+    birthday: "1990-01-01",
+    national: "Viet Nam",
+    sex: "Nam",
+    address: "Dĩ An, Bình Dương",
+    email: "anguyen@gmail.com",
+    department: "Ngoại",
+    phone: "0919000111",
+    position: "Bác sĩ",
+    degree: "PGS.Tiến sĩ",
+    status: "Đang làm việc"
+};
+
+document.getElementById('fullname').textContent = info.fullName;
+document.getElementById('ID').textContent = info.identityNumber;
+document.getElementById('birthday').textContent = info.birthday;
+document.getElementById('national').textContent = info.national;
+document.getElementById('sex').textContent = info.sex;
+document.getElementById('address').textContent = info.address;
+document.getElementById('email').textContent = info.email;
+document.getElementById('department').textContent = info.department;
+document.getElementById('phone').textContent = info.phone;
+document.getElementById('position').textContent = info.position;
+document.getElementById('degree').textContent = info.degree;
+document.getElementById('status').textContent = info.status;
